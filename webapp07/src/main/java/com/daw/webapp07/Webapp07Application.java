0deadec8edb9fb60abcc7706db6831eb494a9752
Pablo R. P. <pablorepost@gmail.com>
2024-02-16 14:54:39 +0100

    spring template

---
+++ webapp07/src/main/java/com/daw/webapp07/Webapp07Application.java
@@ -0,0 +1,13 @@
+package com.daw.webapp07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webapp07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webapp07Application.class, args);
+	}
+
+}
---
+++ webapp07/src/test/java/com/daw/webapp07/Webapp07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daw.webapp07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webapp07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
