f1f6350048df58295c301903c9a87eef544b7284
Pablo R. P. <pablorepost@gmail.com>
2024-02-16 18:19:59 +0100

    user controller not working

--- src/main/java/com/daw/webapp07/User.java
+++ src/main/java/com/daw/webapp07/User.java
@@ -8,4 +8,59 @@ public class User {
     private int HashedPassword;
     private ArrayList<Inversion> inversions;
     private ArrayList<Project> projects;
+
+    public User(String name, String email, int HashedPassword) {
+        this.name = name;
+        this.email = email;
+        this.HashedPassword = HashedPassword;
+        this.inversions = new ArrayList<>();
+        this.projects = new ArrayList<>();
+    }
+
+    public User(){
+        this.inversions = new ArrayList<>();
+        this.projects = new ArrayList<>();
+    }
+
+    public void addInversion(Inversion i){
+        inversions.add(i);
+    }
+
+    public void addProject(Project p){
+        projects.add(p);
+    }
+
+    public ArrayList<Inversion> getInversions(){
+        return inversions;
+    }
+
+    public ArrayList<Project> getProjects(){
+        return projects;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getHashedPassword() {
+        return HashedPassword;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setHashedPassword(int HashedPassword) {
+        this.HashedPassword = HashedPassword;
+    }
+
+
 }
--- src/main/java/com/daw/webapp07/UserController.java
+++ src/main/java/com/daw/webapp07/UserController.java
@@ -1,8 +1,23 @@
 package com.daw.webapp07;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("user", new User());
+        return "header";
+    }
+
+    @PostMapping("/login")
+    public String loginSubmit(Model model, User user) {
+        System.out.println(user.getName());
+        return "inner-page";
+    }
+
 
 }
