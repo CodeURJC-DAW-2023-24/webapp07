b44b6425eae3bfc6ec1e8eeddd74b8893a901810
miguel <ma.garciam.2021@alumnos.urjc.es>
2024-02-19 12:16:21 +0100

    Post project

--- src/main/java/com/daw/webapp07/controller/ProjectController.java
+++ src/main/java/com/daw/webapp07/controller/ProjectController.java
@@ -2,13 +2,14 @@ package com.daw.webapp07.controller;
 
 import com.daw.webapp07.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import com.daw.webapp07.model.Project;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Optional;
 
@@ -30,6 +31,7 @@ public class ProjectController {
 
     @GetMapping(value={"/inner-page","/"})
     public String innerPage(Model model) {
+
         model.addAttribute("projects", projectRepository.findAll());
         return "inner-page";
     }
@@ -43,6 +45,13 @@ public class ProjectController {
         }
         model.addAttribute("project",project.get());
         return "project-details";
+    }
+
+    @PostMapping("/create-project/new")
+    public String createBook(@RequestBody Project project, Model model) {
+
+        projectRepository.save(project);
+        return "inner-page";
     }
 
 
