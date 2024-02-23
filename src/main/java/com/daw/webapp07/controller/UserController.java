c079bd44062c34f99e64e2b0548d1232b27da238
Diego-G19 <diegofernandezcondee@gmail.com>
2024-02-23 14:56:52 +0100

    login change

--- src/main/java/com/daw/webapp07/controller/UserController.java
+++ src/main/java/com/daw/webapp07/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.List;
 import java.util.Optional;
