9521fe22ff74786976b0ff611e136210f617cad6
Fernando <falonsocalderon@gmail.com>
2024-03-25 20:00:55 +0100

    added UserRestController documentation

--- src/main/java/com/daw/webapp07/Webapp07Application.java
+++ src/main/java/com/daw/webapp07/Webapp07Application.java
@@ -1,7 +1,10 @@
 package com.daw.webapp07;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Webapp07Application {
@@ -10,4 +13,14 @@ public class Webapp07Application {
 		SpringApplication.run(Webapp07Application.class, args);
 	}
 
+	// Customize Swagger
+	/*@Bean
+	public OpenAPI customOpenAPI(){
+		return new OpenAPI()
+				.info(new Info()
+						.title("SeedVentures Spring Boot 3 API")
+						.version("xxx")
+						.description("Api documentation for SeedVentures")
+				);
+	}*/
 }
